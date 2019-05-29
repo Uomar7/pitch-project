4583797978bb2c9b3eb713d6802aad64c0307687
Uomar <uomarearlie7@gmail.com>
2019-05-30 01:33:03 +0300

    done the readme and the start command

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='Umarngare'
+export DATABASE_URL='postgresql+psycopg2://king:Uomarearlie1995localhost/pitch1'
+export MAIL_USERNAME='uomarearlie7@gmail.com'
+export MAIL_PASSWORD='umarngare'
+python3 manage.py server
