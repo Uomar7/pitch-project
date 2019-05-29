671324f26a3cd834105b80a12c42c9ddf3e81a43
Uomar <uomarearlie7@gmail.com>
2019-05-30 00:03:31 +0300

    Created the config file and edited the app configurations

---
+++ config.py
@@ -0,0 +1,53 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://king:Uomarearlie1995localhost/pitch2'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'PITCH'
+    SENDER_EMAIL = os.environ.get('MAIL_USERNAME')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # simplemde confirgurations
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://king:Uomarearlie1995@localhost/pitch_test'
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        Config : the parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://king:Uomarearlie1995@localhost/pitch2'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
