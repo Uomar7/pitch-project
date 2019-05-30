7350a8433eea1cf549df9bc3d8034fdcdf1ba915
Uomar <uomarearlie7@gmail.com>
2019-05-30 13:50:59 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://king:Uomarearlie1995@localhost/pitch3'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://king:Uomarearlie1995@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     # email configurations
